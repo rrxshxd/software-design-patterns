b0332083fac0313bd54f900db7a0d2dd71e048d2
rrxshxd <gajiev.r.05@gmail.com>
2024-10-01 22:15:00 +0500

    Created Prototype pattern

--- src/Singleton/DriverManager.java
+++ src/Singleton/DriverManager.java
@@ -20,11 +20,4 @@ public class DriverManager {
         System.out.println("Nearest driver found: " + "Driver1");
         return "Driver1";
     }
-
-    public static void main(String[] args) {
-        Passenger passenger = new Passenger("Rashad");
-        DriverManager driverManager = DriverManager.getInstance();
-        String driver = driverManager.findNearestDriver("Location A");
-        passenger.assignTaxi(driver);
-    }
 }
