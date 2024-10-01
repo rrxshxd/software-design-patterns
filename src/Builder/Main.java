f07384a8c1366ca72439499e7f089a5a2b9205c7
rrxshxd <gajiev.r.05@gmail.com>
2024-10-01 23:16:47 +0500

    Created Builder pattern

---
+++ src/Builder/Main.java
@@ -0,0 +1,15 @@
+package Builder;
+
+public class Main {
+    public static void main(String[] args) {
+        Driver driver = new Driver.DriverBuilder()
+                .withName("Johny Silverhand")
+                .withCarModel("Porshe 911")
+                .withLicenseNumber("XYZ12345")
+                .withExperienceYears(5)
+                .isAvailable(true)
+                .build();
+
+        System.out.println(driver.toString());
+    }
+}
